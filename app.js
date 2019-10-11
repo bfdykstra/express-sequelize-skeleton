f2a92dd5eb5a5eb3007176a1279d4a8a5cf89048
Ben Dykstra <bfdykstra@email.wm.edu>
2019-10-11 16:48:01 -0400

    catch 404 for favicon.ico

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +32,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('Not Found')
 });
 
 module.exports = app;
